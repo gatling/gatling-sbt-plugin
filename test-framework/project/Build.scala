3d13714d13220ce560bba74aae606396602860f7
Dmytro Makhno <makh.dv@gmail.com>
2013-09-11 09:39:19 +0300

    allow local publish for debug

--- test-framework/project/Build.scala
+++ test-framework/project/Build.scala
@@ -5,24 +5,26 @@ object GTFBuild extends Build {
 
   lazy val gatlingProject = Project("gatling-sbt", file("."), settings = gatlingTestFrameworkSettings)
 
-  val gatlingVersion = "2.0.0-SNAPSHOT"
-  val gatlingVersionHC = "2.0.0-M3"
+  val gatlingVersion = "2.0.0-M3a"
+  val gatlingVersionHC = "2.0.0-M3a"
 
   val gatlingTestFrameworkSettings = Defaults.defaultSettings ++ Seq(
     scalaVersion := "2.10.1",
     organization := "gatling",
     name := "gatling-sbt-test-framework",
     version := "0.0.1-SNAPSHOT",
+    //todo: revisit, on final versions
+    publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))),
 
     resolvers ++= Seq(
       "Local Maven Repository" at file(Path.userHome.absolutePath+"/.m2/repository").toURI.toURL.toString,
       "Gatling Releases Repo" at "http://repository.excilys.com/content/repositories/releases",
       "Gatling Snaps Repo" at "http://repository.excilys.com/content/repositories/snapshots",
       "Gatling Third-Party Repo" at "http://repository.excilys.com/content/repositories/thirdparty"
+
   ),
     libraryDependencies ++= Seq(
       "org.scala-tools.testing" % "test-interface" % "0.5",
-      //"com.typesafe.akka" %% "akka-actor" % "2.2.0",
       "io.gatling" % "gatling-app" % gatlingVersion ,
       "io.gatling" % "gatling-core" % gatlingVersion ,
       "io.gatling" % "gatling-http" % gatlingVersion ,
