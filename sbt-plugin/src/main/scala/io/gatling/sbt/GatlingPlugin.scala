570b942c66a4028309aecffa6a5149125a29e921
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-05-13 23:38:56 +0200

    Add settings for integration tests, close #10

--- sbt-plugin/src/main/scala/io/gatling/sbt/GatlingPlugin.scala
+++ sbt-plugin/src/main/scala/io/gatling/sbt/GatlingPlugin.scala
@@ -8,24 +8,46 @@ import io.gatling.sbt.GatlingTasks._
 
 object GatlingPlugin extends Plugin {
 
+  /*************/
+  /** Configs **/
+  /*************/
+
   val Gatling = config("gatling") extend Test
+
+  val GatlingIt = config("gatling-it") extend IntegrationTest
+
+  /*******************************/
+  /** Test framework definition **/
+  /*******************************/
 
   val gatlingTestFramework = TestFramework("io.gatling.sbt.GatlingFramework")
 
-  lazy val gatlingSettings = inConfig(Gatling)(gatlingBaseSettings)
+  /**************/
+  /** Settings **/
+  /**************/
 
-  lazy val gatlingBaseSettings = Defaults.testSettings ++ Seq(
-    testFrameworks in Gatling := Seq(gatlingTestFramework),
-    target in Gatling := target.value / "gatling",
-    testOptions in Gatling += Argument("-m", "-rf", (target in Gatling).value.getPath),
-    sourceDirectory in Gatling := (sourceDirectory in Test).value,
-    parallelExecution in Gatling := false,
-    fork in Gatling := true,
-    testGrouping in Gatling := (definedTests in Gatling).value map singleTestGroup,
-    startRecorder in Gatling := recorderRunner((dependencyClasspath in Test).value.map(_.data), organization.value, (scalaSource in Gatling).value),
-    lastReport in Gatling := openLastReport((target in Gatling).value),
-    clean in Gatling := cleanReports((target in Gatling).value))
+  lazy val gatlingSettings = inConfig(Gatling)(Defaults.testSettings ++ gatlingBaseSettings(Gatling, Test))
 
-  def singleTestGroup(test: TestDefinition) = new Group(test.name, Seq(test), SubProcess(ForkOptions()))
+  lazy val gatlingItSettings = inConfig(GatlingIt)(Defaults.itSettings ++ gatlingBaseSettings(GatlingIt, IntegrationTest))
+
+  lazy val gatlingAllSettings = gatlingSettings ++ gatlingItSettings
+
+  /********************/
+  /** Helper methods **/
+  /********************/
+
+  private def gatlingBaseSettings(config: Configuration, parent: Configuration) = Seq(
+    testFrameworks in config := Seq(gatlingTestFramework),
+    target in config := target.value / config.name,
+    testOptions in config += Argument("-m", "-rf", (target in config).value.getPath),
+    sourceDirectory in config := (sourceDirectory in parent).value,
+    parallelExecution in config := false,
+    fork in config := true,
+    testGrouping in config := (definedTests in config).value map singleTestGroup,
+    startRecorder in config := recorderRunner((dependencyClasspath in parent).value.map(_.data), organization.value, (scalaSource in config).value),
+    lastReport in config := openLastReport((target in config).value),
+    clean in config := cleanReports((target in config).value))
+
+  private def singleTestGroup(test: TestDefinition) = new Group(test.name, Seq(test), SubProcess(ForkOptions()))
 
 }
--- sbt-test/project/TestBuild.scala
+++ sbt-test/project/TestBuild.scala
@@ -1,18 +1,18 @@
 import sbt._
 import sbt.Keys._
 
-import io.gatling.sbt.GatlingPlugin.{ Gatling, gatlingSettings }
+import io.gatling.sbt.GatlingPlugin._
 
 object TestBuild extends Build {
 
 val libs = Seq(
-	"io.gatling.highcharts" % "gatling-charts-highcharts" % "2.0.0-SNAPSHOT" % "test",
-	"io.gatling" % "test-framework" % "1.0-SNAPSHOT" % "test"
+	"io.gatling.highcharts" % "gatling-charts-highcharts" % "2.0.0-SNAPSHOT" % "it,test" ,
+	"io.gatling" % "test-framework" % "1.0-SNAPSHOT" % "it,test"
 	)
 
 	val root = Project("sbt-test", file("."))
-				.settings(gatlingSettings: _*)
-				.configs(Gatling)
+				.settings(gatlingAllSettings: _*)
+				.configs(IntegrationTest, Gatling, GatlingIt)
 				.settings(organization := "io.gatling.sbt.test")
 				.settings(libraryDependencies ++= libs)
 }
