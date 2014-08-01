b2102db648329be7de0e71875d7f7ab69ed1c157
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-08-01 15:56:57 +0200

    Add missing scm + developers configuration for Sonatype

--- project/Publish.scala
+++ project/Publish.scala
@@ -9,14 +9,48 @@ import Resolvers._
 object Publish {
 
   lazy val sonatypeSettings = Seq(
-    publishMavenStyle := true,
-    credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
-    publishTo := Some(if(isSnapshot.value) sonatypeSnapshots else sonatypeStaging)
+    publishMavenStyle    := true,
+    pomExtra             := scm ++ developersXml(developers),
+    pomIncludeRepository := { _ => false },
+    credentials          += Credentials(Path.userHome / ".sbt" / ".credentials"),
+    publishTo            := Some(if(isSnapshot.value) sonatypeSnapshots else sonatypeStaging)
   )
 
-  lazy val bintraySettings = Seq(
+  lazy val bintraySettings = bintrayPublishSettings ++ Seq(
     publishMavenStyle := false,
     bintrayOrganization in bintray := None,
     repository in bintray := "sbt-plugins"
-  ) ++ bintrayPublishSettings
+  )
+
+  /************************/
+  /** POM extra metadata **/
+  /************************/
+
+  private val scm = {
+    <scm>
+      <connection>scm:git:git@github.com:gatling/gatling-sbt.git</connection>
+      <developerConnection>scm:git:git@github.com:gatling/gatling-sbt.git</developerConnection>
+      <url>https://github.com/gatling/gatling-sbt</url>
+      <tag>HEAD</tag>
+    </scm>
+  }
+
+  private case class GatlingDeveloper(emailAddress: String, name: String, isEbiz: Boolean)
+
+  private val developers = Seq(GatlingDeveloper("pdalpra@excilys.com", "Pierre Dal-pra", true))
+
+  private def developersXml(devs: Seq[GatlingDeveloper]) = {
+    <developers>
+      {
+      for(dev <- devs)
+      yield {
+        <developer>
+          <id>{dev.emailAddress}</id>
+          <name>{dev.name}</name>
+          { if (dev.isEbiz) <organization>eBusiness Information, Excilys Group</organization> }
+        </developer>
+      }
+      }
+    </developers>
+  }
 }
