b0735a99e054fe51e7d20c2731a0771ab1b99215
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-09-18 23:43:24 +0200

    Cleanup release settings

--- project/Release.scala
+++ project/Release.scala
@@ -1,37 +1,13 @@
-import sbt._
-import sbt.Keys._
-
 import com.typesafe.sbt.SbtPgp.PgpKeys._
-import sbtrelease._
 import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleasePlugin.ReleaseKeys._
-import sbtrelease.ReleaseStateTransformations._
 
 object Release {
 
   lazy val releaseToCentralSettings = releaseSettings ++ Seq(
     crossBuild := false,
-    releaseProcess := Seq[ReleaseStep](
-      checkSnapshotDependencies,
-      inquireVersions,
-      runClean,
-      runTest,
-      setReleaseVersion,
-      commitReleaseVersion,
-      tagRelease,
-      publishSignedStep,
-      setNextVersion,
-      commitNextVersion,
-      pushChanges
-    )
+    publishArtifactsAction := publishSigned.value
   )
   
   lazy val releaseToBintraySettings = releaseSettings
-
-  private val publishSignedStep = ReleaseStep(
-    action = st => {
-      val extracted = Project.extract(st)
-      val ref = extracted.get(thisProjectRef)
-      extracted.runAggregated(publishSigned in Global in ref, st)
-    })
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,6 +10,6 @@ addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 
 addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
