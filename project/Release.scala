560e124f6b922feadb96f3da3be0c36a2a11fe7d
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-09-20 09:55:31 +0200

    Add possibility to pass release info as sys props

--- project/Release.scala
+++ project/Release.scala
@@ -1,13 +1,20 @@
+import scala.util.Properties.propOrEmpty
+
 import com.typesafe.sbt.SbtPgp.PgpKeys._
 import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleasePlugin.ReleaseKeys._
 
 object Release {
 
-  lazy val releaseToCentralSettings = releaseSettings ++ Seq(
+  private lazy val commonReleaseSettings = releaseSettings ++ Seq(
+    releaseVersion := { _ => propOrEmpty("releaseVersion")},
+    nextVersion := { _ => propOrEmpty("developmentVersion")}
+  )
+
+  lazy val releaseToCentralSettings = commonReleaseSettings ++ Seq(
     crossBuild := false,
     publishArtifactsAction := publishSigned.value
   )
   
-  lazy val releaseToBintraySettings = releaseSettings
+  lazy val releaseToBintraySettings = commonReleaseSettings
 }
