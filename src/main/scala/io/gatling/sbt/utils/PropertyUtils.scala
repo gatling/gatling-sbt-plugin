24968451f0afa56195137e375852d963cdcdb374
Stephane Landelle <slandelle@gatling.io>
2016-01-21 21:59:36 +0100

    Clean up Java options

--- src/main/scala/io/gatling/sbt/utils/PropertyUtils.scala
+++ src/main/scala/io/gatling/sbt/utils/PropertyUtils.scala
@@ -18,9 +18,8 @@ package io.gatling.sbt.utils
 object PropertyUtils {
 
   val DefaultJvmArgs = List(
-    "-server", "-XX:+UseThreadPriorities", "-XX:ThreadPriorityPolicy=42", "-Xms512M",
-    "-Xmx512M", "-Xmn100M", "-XX:+HeapDumpOnOutOfMemoryError", "-XX:+AggressiveOpts",
-    "-XX:+OptimizeStringConcat", "-XX:+UseFastAccessorMethods", "-XX:+UseParNewGC",
+    "-server", "-Xms512M", "-Xmx512M", "-XX:+HeapDumpOnOutOfMemoryError",
+    "-XX:+AggressiveOpts", "-XX:+OptimizeStringConcat", "-XX:+UseParNewGC",
     "-XX:+UseConcMarkSweepGC", "-XX:+CMSParallelRemarkEnabled"
   )
 }
