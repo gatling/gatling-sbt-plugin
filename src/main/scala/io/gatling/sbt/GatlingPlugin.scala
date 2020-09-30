gatling/gatling-sbt-plugin

commit 0afb0a35c983b3ca29e0fa663309195f78716f93

Fix duplicated compilation of Gatling simulation for 'gatling' configuration, close gatling/gatling#3490
This is caused by the `gatling` configuration rewiring too much of the
test settings into its configuration, while still linking some of its
setting to the `test` configuration.
This PR replaces `Defaults.testSettings` by `Defaults.testTasks`, which
wires less settings into the `gatling` configuration while still
achieving what we require (eg. splitting Gatling simulations from other
unit tests and integration tests).

src/main/scala/io/gatling/sbt/GatlingPlugin.scala
@@ -42,12 +42,12 @@ object GatlingPlugin extends AutoPlugin {
   /**************/
   /** Settings **/
   /**************/
-  lazy val gatlingSettings =
-    inConfig(Gatling)(Defaults.testSettings ++ gatlingBaseSettings(Gatling, Test))
+  lazy val gatlingSettings: Seq[Def.Setting[_]] =
+    inConfig(Gatling)(Defaults.testTasks ++ gatlingBaseSettings(Gatling, Test))
 
   lazy val gatlingItSettings: Seq[Def.Setting[_]] =
     inConfig(GatlingIt)(
-      Defaults.itSettings ++ Defaults.testTasks ++ gatlingBaseSettings(GatlingIt, IntegrationTest, filterClasspath = false)
+      Defaults.itSettings ++ Defaults.testTasks ++ gatlingBaseSettings(GatlingIt, IntegrationTest)
     )
 
   lazy val gatlingAllSettings: Seq[Def.Setting[_]] =
@@ -56,17 +56,11 @@ object GatlingPlugin extends AutoPlugin {
   /********************/
   /** Helper methods **/
   /********************/
-  private def gatlingBaseSettings(config: Configuration, parent: Configuration, filterClasspath: Boolean = true) = Seq(
+  private def gatlingBaseSettings(config: Configuration, parent: Configuration) = Seq(
     testFrameworks in config := Seq(gatlingTestFramework),
     target in config := target.value / config.name,
-    fullClasspath in config := {
-      val path = (fullClasspath in parent).value
-      val dir = (classDirectory in config).value
-      if (filterClasspath) path.filterNot(_.data == dir) else path
-    },
     testOptions in config += Argument(gatlingTestFramework, "-rf", (target in config).value.getPath),
     javaOptions in config ++= overrideDefaultJavaOptions(),
-    sourceDirectory in config := (sourceDirectory in parent).value,
     parallelExecution in config := false,
     fork in config := true,
     testGrouping in config := (testGrouping in config).value flatMap singleTestGroup,

commit 14eadea4ec9cff71679509238ebf811a2aecd433

Add missing forkOptions in gatling and gatlingIt settings, close gatling/gatling#3971
Motivation:

https://github.com/gatling/gatling-sbt-plugin/commit/0afb0a35c983b3ca29e0fa663309195f78716f93 introduced a regression where forkOptions (in particular Java options) are no longer propagated.

Modification:

Propagate forkOptions too.

Result:

Passing parameters as java System props work again.

src/main/scala/io/gatling/sbt/GatlingPlugin.scala
@@ -43,11 +43,18 @@ object GatlingPlugin extends AutoPlugin {
   /** Settings **/
   /**************/
   lazy val gatlingSettings: Seq[Def.Setting[_]] =
-    inConfig(Gatling)(Defaults.testTasks ++ gatlingBaseSettings(Gatling, Test))
+    inConfig(Gatling)(
+      Defaults.testTasks ++
+        (forkOptions := Defaults.forkOptionsTask.value) ++
+        gatlingBaseSettings(Gatling, Test)
+    )
 
   lazy val gatlingItSettings: Seq[Def.Setting[_]] =
     inConfig(GatlingIt)(
-      Defaults.itSettings ++ Defaults.testTasks ++ gatlingBaseSettings(GatlingIt, IntegrationTest)
+      Defaults.itSettings ++
+        Defaults.testTasks ++
+        (forkOptions := Defaults.forkOptionsTask.value) ++
+        gatlingBaseSettings(GatlingIt, IntegrationTest)
     )
 
   lazy val gatlingAllSettings: Seq[Def.Setting[_]] =